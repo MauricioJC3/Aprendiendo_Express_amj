28171cf1401658864229afab69434174ee2f23af
Andres Mauricio Jimenez Chavez <142183331+MauricioJC3@users.noreply.github.com>
2024-11-13 18:00:01 -0500

    feat(): learning express

--- app/controllers/empresasController.js
+++ app/controllers/empresasController.js
@@ -1,33 +1,34 @@
 const db = require('../../database/conexion.js');
 
-class ProveedoresController {
+
+class EmpresasController {
     constructor() {
 
     }
 
     // Todos los métodos que se utilizan en la ruta tienen dos parámetros req, res 
     consultar(req, res) {
-            res.json({msg: 'consulta de proveedores'});
+            res.json({msg: 'consulta de empresas'});
     }
 
     consultarDetalle(req, res) {
        const { id } = req.params; // desestructuracion
-        res.json({msg: `consulta de un proveedor por id ${id}`});
+        res.json({msg: `consulta de un empresa por id ${id}`});
     }
 
     ingresar(req, res) {
-        res.json({msg: 'ingresar un proveedor'});
+        res.json({msg: 'ingresar un empresa'});
     }
 
     actualizar(req, res) {
-        res.json({msg: 'actualizar un proveedor'});
+        res.json({msg: 'actualizar un empresa'});
     }
 
     eliminar(req, res) {
-        res.json({msg: 'eliminar un proveedor'});
+        res.json({msg: 'eliminar un empresa'});
     }
 
 }
 
 // nueva instancia de la clase
-module.exports = new ProveedoresController();
+module.exports = new EmpresasController();
